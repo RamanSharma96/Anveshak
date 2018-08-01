1c951b6360ce64cbc10e35143d5828d63c304b3e
shara38 <shara38@LT9R8J4Q2.ca.com>
2018-08-01 14:45:23 +0530

    Insertdata business code

---
+++ src/DB/InsertData.java
@@ -0,0 +1,49 @@
+package DB;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.*;
+
+public class InsertData {
+
+	public static void insertData(String CPUusage, String hostname, String ip, String os, String mac, Connection conn) {
+		// TODO Auto-generated method stub
+		  
+		
+		System.out.println("CPUusage: "+ip);
+        
+        
+        //DATABASE CODE
+	    try {
+        if(conn!=null) {
+        Thread.sleep(1000);
+        CallableStatement ps=conn.prepareCall("call updatedevices(?,?,?) ");
+		ps.setString(1, mac);
+		ps.setString(2, os);
+		ps.setString(3, ip);
+	    ps.executeUpdate();
+	    int s=ps.executeUpdate();
+        //System.out.println(s+" ROWS inserted!!!!");
+        
+        Thread.sleep(1000);
+		ps=conn.prepareCall("call insertintocpu(?,?)");
+		ps.setString(1,ip);
+		ps.setString(2, CPUusage);
+		s=ps.executeUpdate();
+		System.out.println(s+" ROWS inserted!!!!");
+        }
+        else {
+        	System.out.println("NO DB CONNECTION");
+        }
+	    }
+	    catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+		
+	}
+
+
+	
+}
